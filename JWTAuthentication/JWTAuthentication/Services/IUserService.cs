fdd9c9376a640d02a5d954e23349efd71208e7dd
funpypy9x <vuminhcuongckpt@gmail.com>
2021-08-30 10:49:52 +0700

    up statged

---
+++ JWTAuthentication/JWTAuthentication/Services/IUserService.cs
@@ -0,0 +1,30 @@
+﻿using JWTAuthentication.Models;
+using JWTAuthentication.Settings;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuthentication.Services
+{
+    public interface IUserService
+    {
+    }
+
+    public class UserService : IUserService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserService(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IOptions<JWT> jwt)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _jwt = jwt.Value;
+        }
+
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly JWT _jwt;
+    }
+}
